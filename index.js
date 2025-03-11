9b571b9ff0abea13f97a6513de1e823fd1ab5e0a
Emmakaranja1 <karanjaemmak@gmail.com>
2025-03-11 15:52:55 +0300

    Removes redundant global event listener for button click,Updates Event Binding method, Ensures DOM is fully loaded before binding event listeners

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// Function to add an event listener to a button
 function addingEventListener() {
-    const input = document.getElementById('button');
-    input.addEventListener('click', function() {
-      alert('I was clicked!');
-    });
-}
+// Get the button element by its ID 'button'
 const input = document.getElementById('button');
 
+// Add a click event listener to the button
+input.addEventListener('click', function() {
+// This function will be executed when the button is clicked
+ alert('I was clicked!');// Display an alert when the button is clicked
+    });
+}
+// Get the button element by its ID 'button' and store it in the input variable
+const input = document.getElementById('button');
+
+// Function to display an alert when called
 function clickAlert() {
-  alert('I was clicked!');
+  alert('I was clicked!');// Display an alert with a message
 }
 
-input.addEventListener('click', clickAlert);
+// Add a second event listener to the same button to call clickAlert when clicked
+input.addEventListener('click', clickAlert); // This will also alert 'I was clicked!'
